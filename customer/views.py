27777e5e36c376b9dd8298fe3e6854a7866591d4
Samuel Magafu <mac@code29.local>
2020-04-01 16:31:43 +0300

    worked on customer data

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import CustomerHouseHold
+from . models import CustomerHouseHold, Transaction, MetreReading
 from django.views.generic import ListView,DetailView
 
 class CustomersListView(ListView):
@@ -11,4 +11,17 @@ class CustomersListView(ListView):
 class CustomerDetailView(DetailView):
     model = CustomerHouseHold
     template_name = 'customers/details.html'
+    context_object_name = 'client'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['transaction'] = Transaction.objects.filter(buyer=self.get_object()).order_by('-date')[:10]
+        context['meterreading'] = MetreReading.objects.filter(owner=self.get_object()).order_by('-date')[:10]
+        return context
+
+
 # Create your views here.
+# def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['category'] = Category.objects.all()
+#         return context
